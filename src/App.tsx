e9098a4992048ce466c104278af28f6afd4b464c
Hilverton Cesar <cesaramojesus@hotmail.com>
2021-12-28 20:58:07 -0300

    🎉 Configurações iniciais

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <div className="container pt-4">
+      <h1 className="mb-4">Iniciando ELECTRON + REACT + TYPESCRIPT</h1>
+      <i
+        className="bi-alarm"
+        style={{ fontSize: '2rem', color: 'cornflowerblue' }}
+      />
+    </div>
+  )
+}
