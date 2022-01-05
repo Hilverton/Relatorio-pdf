2976456f05cb7a98913a2a06af9aae33e7a6408c
Hilverton Cesar <cesaramojesus@hotmail.com>
2022-01-05 08:49:52 -0300

    ✨ Adição de dados dinâmicos à página

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,3 +1,4 @@
+import { useState, ChangeEvent } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import Layout from '../components/templates/Layout'
@@ -10,10 +11,31 @@ import TableWithAddition from '../components/organisms/TableWithAddition'
 
 const months = ['Janeiro', 'Fevereiro', 'Março']
 
+interface DataPage {
+  month: string
+  starting_day: string
+  final_day: string
+}
+
+const INITIAL_DATA = {
+  month: '',
+  starting_day: '',
+  final_day: '',
+}
+
 export default function Report() {
   const history = useHistory()
+  const [infos, setInfos] = useState<DataPage>(INITIAL_DATA)
 
   const handleGoBack = () => history.replace('/')
+
+  function InfosDoc(event: ChangeEvent<HTMLInputElement>) {
+    setInfos({ ...infos, [event.target.name]: event.target.value })
+  }
+
+  function getData() {
+    console.log('infos', infos)
+  }
 
   return (
     <Layout>
@@ -22,9 +44,27 @@ export default function Report() {
         <section className="flex-col space-y-4">
           <SectionTitle>Informações do documento</SectionTitle>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            <DataList label="Digite o mês" name="month" dataList={months} />
-            <Input label="Dia inicial" name="starting_day" type="date" />
-            <Input label="Dia final" name="final_day" type="date" />
+            <DataList
+              label="Digite o mês"
+              name="month"
+              dataList={months}
+              value={infos.month}
+              onChange={InfosDoc}
+            />
+            <Input
+              label="Dia inicial"
+              name="starting_day"
+              type="date"
+              value={infos.starting_day}
+              onChange={InfosDoc}
+            />
+            <Input
+              label="Dia final"
+              name="final_day"
+              type="date"
+              value={infos.final_day}
+              onChange={InfosDoc}
+            />
           </div>
         </section>
         <section className="flex-col space-y-4">
@@ -35,7 +75,9 @@ export default function Report() {
           <Button hasOutline bgColor="delete" onClick={handleGoBack}>
             Voltar
           </Button>
-          <Button bgColor="success">Gerar Arquivo</Button>
+          <Button bgColor="success" onClick={getData}>
+            Gerar Arquivo
+          </Button>
         </section>
       </div>
     </Layout>
