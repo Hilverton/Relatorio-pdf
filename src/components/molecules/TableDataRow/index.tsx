5ef5227e75f2f89475209355405c4f3b31cacc19
Hilverton Cesar <cesaramojesus@hotmail.com>
2022-01-05 10:01:47 -0300

    🔥 Mudança de layout: remoção do input na tabela

--- src/components/molecules/TableDataRow/index.tsx
+++ src/components/molecules/TableDataRow/index.tsx
@@ -1,25 +1,11 @@
 import TableRowHead from '../../atoms/TableRowHead'
 import TableData from '../../atoms/TableData'
 import Button from '../../atoms/Button'
-import Input from '../../atoms/Input'
 
 export default function TableDataRow({ headers, datas, idx }: TableDataRow) {
   return (
     <TableRowHead isEven={idx % 2 === 0}>
       {headers.map((header, index) => {
-        if (header === 'Valor') {
-          return (
-            <TableData key={index} width="w-full md:w-52">
-              <div className="flex">
-                <span className="inline-block w-1/3 md:hidden font-bold">
-                  Valor
-                </span>
-                <Input name="input_value" type="text" label="" />
-              </div>
-            </TableData>
-          )
-        }
-
         if (header === 'Ações') {
           return (
             <TableData key={index} width="w-full md:w-52">
